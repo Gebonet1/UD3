e82a016a58527fb7d18cbcbb30d9412b839cc854
Gebonet1 <78427755+Gebonet1@users.noreply.github.com>
2021-02-02 17:49:10 +0100

    Primera acció

---
+++ UD3/src/TEST/ComentarioLinea.java
@@ -0,0 +1,10 @@
+package TEST;
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println(" Hola que tal ")
+		  System.out.println("Adios ");
+		  System.out.println("FIN DE PROGRAMA ")
+		;}
+		}
+ 
---
+++ UD3/src/TEST/ComentarioVariasLineas.java
@@ -0,0 +1,13 @@
+package TEST;
+public class ComentarioVariasLineas {
+		public static void main (String[]args){
+		/*System.out.println("Hola ");
+		  System.out.println("Adios ");
+		  System.out.println("Cucu");*/
+		for(int i=0;i<6;/*ahora viene el passo*/ i++)
+		   System.out.println(i+""); 
+		   System.out.println("FIN DE PROGRMA")
+		;}
+
+		}
+
